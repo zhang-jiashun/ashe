f743e24c5f68b4f636009006b26ed1cdf85d70f3
zhangjiashun <jiashun.zhang@qq.com>
2019-10-26 11:14:59 +0800

    137_创建笔记

--- app/controllers/admin/notes_controller.rb
+++ app/controllers/admin/notes_controller.rb
@@ -3,5 +3,26 @@ module Admin
     def index
       @notes = Note.all
     end
+
+    def new
+      @note = Note.new
+    end
+
+    def create
+      @note = Note.new(note_params)
+      if @note.save
+        flash[:notice] = '新建成功！'
+        redirect_to admin_notes_path
+      else
+        flash[:notice] = @note.errors.full_messages[0]
+        render :new
+      end
+    end
+
+    private
+
+    def note_params
+      params.require(:note).permit(:title, :body)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :notes, only: [:index]
+    resources :notes, only: [:index, :new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
