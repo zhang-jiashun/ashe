f3b279d678bb140107e7b5ae150614da8ac372fe
zhangjiashun <jiashun.zhang@qq.com>
2019-10-27 22:31:38 +0800

    139_登陆，退出

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,24 @@
+module Admin
+  class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+      author = Author.find_by(email: params[:email])
+      if author&.authenticate(params[:password])
+        session[:author_id] = author.id
+        flash[:success] = 'Successfully logged in !'
+        redirect_to admin_notes_url
+      else
+        flash[:notice] = 'Invalid email or password'
+        redirect_to [:admin, :login]
+      end
+    end
+
+    def destroy
+      reset_session
+      flash[:notice] = 'Successfully logged out !'
+      redirect_to [:admin, :login]
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :notes
+    resources :sessions
+
+    get 'login' => 'sessions#new'
+    post 'login' => 'sessions#create'
+    get 'logout' => 'sessions#destroy'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
