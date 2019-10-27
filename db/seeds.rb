2960b476a614286a768561318ef536914162217d
zhangjiashun <jiashun.zhang@qq.com>
2019-10-27 21:54:30 +0800

    139_添加 Author seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Author.create!(email: 'js@qq.com', password: 'password')
